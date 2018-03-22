d4b5c37bbb96e73117c088a93ac99c50e973e186
Alexey Shishkov <cookies.eater.03@gmail.com>
2018-03-23 01:25:48 +0300

    Local params

--- config/params.php
+++ config/params.php
@@ -1,5 +1,10 @@
 <?php
 
-return [
+use yii\helpers\ArrayHelper;
+
+if( is_readable( __DIR__ . '/params.local.php' ) )
+    $local = require( __DIR__ . '/params.local.php' );
+
+return ArrayHelper::merge([
     'adminEmail' => 'admin@example.com',
-];
+], $local);
