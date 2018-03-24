77d77d3750c38f998004fddb743cc278b9d68854
Alexey Shishkov <cookies.eater.03@gmail.com>
2018-03-24 19:32:35 +0300

    Not found page

--- themes/backend/src/pages/NotFoundPage.vue
+++ themes/backend/src/pages/NotFoundPage.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>Not Found</h1>
+  <div class="app flex-row align-items-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <h1 class="float-left display-3 mr-4">404</h1>
+          <h4 class="pt-3">Упс, что-то пошло не так</h4>
+          <p class="text-muted">Такая страница не найдена</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
-<script>
-export default {
-
-};
-</script>
