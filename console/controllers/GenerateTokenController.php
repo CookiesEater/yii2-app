71db9fe1048c79d5756140378275c01d83355a74
Alexey Shishkov <cookies.eater.03@gmail.com>
2018-04-18 16:51:42 +0300

    JwtTokenSecret generating

--- console/controllers/GenerateTokenController.php
+++ console/controllers/GenerateTokenController.php
@@ -68,4 +68,39 @@ class GenerateTokenController extends \yii\console\Controller
         $this->stdout( "Token was successfully generated.\n" );
         return ExitCode::OK;
     }
+
+    /**
+     * @return int
+     * @throws \yii\base\Exception
+     */
+    public function actionJwt()
+    {
+        $config = realpath( __DIR__ . '/../../config/params.local.php' );
+        $this->stdout( "Generate jwt token in file '{$config}' ...\n" );
+        if( !is_file( $config ) )
+        {
+            $this->stderr( "Can't find {$config}" );
+            return ExitCode::IOERR;
+        }
+
+        $count = 0;
+        $token = ( new Security() )->generateRandomString( 32 );
+        $template = '/(("|\')JwtTokenSecret("|\')\s*=>\s*)(""|\'\')/';
+        $templateForce = '/(("|\')JwtTokenSecret("|\')\s*=>\s*)("[^"]+"|\'[^\']+\')/';
+        $content = preg_replace( $this->force ? $templateForce : $template, "\\1'{$token}'", file_get_contents($config), -1, $count );
+
+        if( !$count )
+        {
+            if( $this->force )
+                $this->stderr( "Can't find 'JwtTokenSecret' definition\n" );
+            else
+                $this->stderr( "Can't find 'JwtTokenSecret' definition or it's already filled (use -f to force regenerate)\n" );
+
+            return ExitCode::DATAERR;
+        }
+
+        file_put_contents( $config, $content );
+        $this->stdout( "Token was successfully generated.\n" );
+        return ExitCode::OK;
+    }
 }
